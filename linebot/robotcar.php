5549b34b3c4b8079e81ee2029b080c5c7cc461aa
oaikaoz <oaikaoz@hotmail.com>
2019-07-26 14:15:59 +0700

    add robot car

--- linebot/robotcar.php
+++ linebot/robotcar.php
@@ -1,14 +1,16 @@
 <?php
-
+   // echo "Hello LINE bot World!"; 
+   
+  // limitless-falls-79984.herokuapp.com/robotcar.php
     $API_URL = 'https://api.line.me/v2/bot/message/reply';
     $APIPUSH_URL = 'https://api.line.me/v2/bot/message/push';
     $APIProfile_URL = 'https://api.line.me/v2/bot/profile/';
-    $ACCESS_TOKEN = "q/33PeZ0m0SKUUNr36XYg37LIrz+Qk2uW8pWVddhiNGm0EHDP6knf84R13bxj3FaE29rhdF9V6HnkVcsOS3tFdM5ErEqgPeqD2fqpn66iNTlqPEDmdHDbdGxjMuwdp+G6H+bYgCwzQ7BsboWC39qLAdB04t89/1O/w1cDnyilFU=";
+    $ACCESS_TOKEN = 'iLUzbLrksc3Qs4eno+YIdPSf2RTtX9pKhmC+TBf/zZ50c6AF/N7nVv+K72B5KnWJlGKQUiJ8tVb4zRkMbbjrb4KIpYjeyQ9Ie2ovP/mRNHWDMT3U/gxjueKf953JNzbZ4NmXYBt56DXWkdwmOx6DJAdB04t89/1O/w1cDnyilFU=';
     $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
     $request = file_get_contents('php://input');  
     $request_array = json_decode($request, true); 
     
-    $externalIp = file_get_contents('http://androidserver.ddns.net/line_bot/getrealip.php'); //del
+    $externalIp = file_get_contents('http://androidserver.ddns.net/line_bot/getrealip.php');
     $post_data = http_build_query($request_array);
     $options = array('http' => array(
        'method' => 'POST',
@@ -16,19 +18,13 @@
        'content' => $post_data
      ));
     $context = stream_context_create($options);
-    $result = file_get_contents('http://'.$externalIp.':85/line_bot/connectasset.php', false, $context); // change 127
+    $result = file_get_contents('http://'.$externalIp.':85/line_bot/connectrobotcar.php', false, $context);
     $pushdata2= json_decode($result, true);
  
 
-        if (is_array($pushdata2) || is_object($pushdata2))
-        {
-            foreach ($pushdata2 as  $arr_data) {
-            
-            $push_body2 = json_encode($arr_data, JSON_UNESCAPED_UNICODE);
-            $send_result2 = push_message($APIPUSH_URL, $POST_HEADER, $push_body2);
-                
-            }
-        }
+    $push_body2 = json_encode($arr_data, JSON_UNESCAPED_UNICODE);
+    $send_result2 = send_reply_message($APIPUSH_URL, $POST_HEADER, $push_body2);
+
 
 
     function send_reply_message($url, $post_header, $post_body)
@@ -60,7 +56,7 @@
 
     function GET_Profile($url, $post_header, $post_body)
     {
-        $ch = curl_init($url); 
+        $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
