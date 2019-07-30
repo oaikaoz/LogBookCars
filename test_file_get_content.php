a13f034bba03afaa7fdf5510542583d0004971cd
oaikaoz <oaikaoz@hotmail.com>
2019-07-30 15:06:24 +0700

    update

--- test_file_get_content.php
+++ test_file_get_content.php
@@ -1,12 +1,12 @@
 <?php
 
-$options = array('http' => array(
-    'method' => 'POST',
-    'header' => 'Content-type: application/x-www-form-urlencoded',
-    'content' => $post_data
-  ));
- $context = stream_context_create($options);
- $result = file_get_contents('https://roh2.triplet.co.th/logbookcar/report/index.php', false, $context);
-
- echo $result;
+$arrContextOptions=array(
+    "ssl"=>array(
+          "verify_peer"=>false,
+          "verify_peer_name"=>false,
+      ),
+  );  
+$url  ='https://roh2.triplet.co.th/logbookcar/report/index.php';
+$response = file_get_contents($url, false, stream_context_create($arrContextOptions));
+ echo $response;
 ?>
