a95901cfb9cf32e600c3d80048450ce4ec5bf0db
oaikaoz <oaikaoz@hotmail.com>
2019-07-30 15:55:43 +0700

    ipdate

--- test_file_get_content.php
+++ test_file_get_content.php
@@ -15,37 +15,33 @@ $response = file_get_contents($url, false, stream_context_create($arrContextOpti
  echo $response;
 
 */
- 
 
 
-  header('Content-type: text/html; charset=UTF-8');
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-CURLOPT_URL => "https://roh2.triplet.co.th/index.php",
-CURLOPT_RETURNTRANSFER => true,
-CURLOPT_ENCODING => "",
-CURLOPT_MAXREDIRS => 10,
-CURLOPT_TIMEOUT => 30,
-CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-
-
+  CURLOPT_URL => "https://roh2.triplet.co.th/index.php",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache",
+    "postman-token: 70baa78d-450c-f2dd-bcc1-8f70e6dd7e32"
+  ),
 ));
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-$response = curl_exec($curl);   
+$response = curl_exec($curl);
 $err = curl_error($curl);
 
 curl_close($curl);
 
 if ($err) {
-echo "cURL Error #:" . $err;
+  echo "cURL Error #:" . $err;
 } else {
-echo $response;
-
-
-
+  echo $response;
 }
 
 ?>
