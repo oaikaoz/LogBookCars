e7d270d4b61f44f216902bf46d31c87788a76c34
oaikaoz <oaikaoz@hotmail.com>
2019-07-30 15:10:29 +0700

    update_

--- test_file_get_content.php
+++ test_file_get_content.php
@@ -6,7 +6,7 @@ $arrContextOptions=array(
           "verify_peer_name"=>false,
       ),
   );  
-$url  ='https://roh2.triplet.co.th/logbookcar/report/index.php';
+$url  ='https://roh2.triplet.co.th/index.php';
 $response = file_get_contents($url, false, stream_context_create($arrContextOptions));
  echo $response;
 ?>
