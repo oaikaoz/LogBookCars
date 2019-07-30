7540f82bf52a4eb5df300bc98a3ea4e72ed12096
oaikaoz <oaikaoz@hotmail.com>
2019-07-30 15:50:35 +0700

    UPDATE

--- test_file_get_content.php
+++ test_file_get_content.php
@@ -1,5 +1,9 @@
+
+
+
 <?php
 
+/*
 $arrContextOptions=array(
     "ssl"=>array(
           "verify_peer"=>false,
@@ -9,4 +13,39 @@ $arrContextOptions=array(
 $url  ='https://roh2.triplet.co.th/index.php';
 $response = file_get_contents($url, false, stream_context_create($arrContextOptions));
  echo $response;
+
+*/
+ 
+
+
+  header('Content-type: text/html; charset=UTF-8');
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+CURLOPT_URL => "https://roh2.triplet.co.th/index.php",
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_ENCODING => "",
+CURLOPT_MAXREDIRS => 10,
+CURLOPT_TIMEOUT => 30,
+CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+
+
+));
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$response = curl_exec($curl);   
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+echo "cURL Error #:" . $err;
+} else {
+echo $response;
+
+
+
+}
+
 ?>
